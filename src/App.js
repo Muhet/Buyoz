5b79da307f2c98ca65308d0f59f3821d2ec544db
Muhet <muhedarius96@gmail.com>
2022-10-06 05:17:37 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar } from './components';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  {/*const [cart, setCart] = useState({});*/}
+    
+    const fetchProducts = async () => {
+      const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+      {/*const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+      }*/}
+      {/*const handleAddCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity); 
+
+          setCart(item.cart);
+      }*/}
+      
+
+    useEffect(() => {
+      fetchProducts();
+    {/*fetchCart();*/}
+    }, []);
+    console.log(products);
+  return (
+    <div>
+      <Navbar />
+     <Products products={products}/>
+      {/*<Cart cart={cart}/>*/}
+    </div>
+  )
+}
+
+export default App;
+
