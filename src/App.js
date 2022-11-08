20e25281f7f1638e106a21d1fc3b22fad1046836
NIYODUSENGA Clement <clementmistico@gmail.com>
2022-11-08 10:36:00 +0200

    deleted .env

--- src/App.js
+++ src/App.js
@@ -4,86 +4,84 @@ import { Products, Navbar, Cart, Checkout, Blog } from './components';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
   const [products, setProducts] = useState([]);
- const [cart, setCart] = useState({});
- const { order, setOrder } = useState({});
- const { errorMessage, seterrorMessage } = useState('');
-    
-    const fetchProducts = async () => {
-      const { data } = await commerce.products.list();
+  const [cart, setCart] = useState({});
+  const { order, setOrder } = useState({});
+  const { errorMessage, seterrorMessage } = useState('');
 
-        setProducts(data);
-    }
-      
-      const fetchCart = async () => { 
-        setCart(await commerce.cart.retrieve())
-      }
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
-      const handleAddCart = async (productId, quantity) => {
-          const { cart }= await commerce.cart.add(productId, quantity);
-             setCart(cart);
-      }
+    setProducts(data);
+  }
 
-      const handleUpdateCartQty = async (productId, quantity) => {
-          const { cart } = await commerce.cart.update(productId, {quantity});
-            setCart(cart)
-      }
-    const handleRemoveFromCart = async (productId) => {
-        const { cart } = await commerce.cart.remove(productId);
-            setCart(cart);
-    }
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
 
-    const handleEmptyCart = async () => {
-          const { cart } = await commerce.cart.empty();
-          setCart(cart);
-    }
-      const refleshCart = async () => {
-        const newCart = await commerce.cart.refresh();
-        setCart(newCart);
-      }
-    const handleCapttureCheckout = async (checkoutTokenId, newOrder) =>{
-          try {
-              const incommingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
-              setOrder(incommingOrder);
-              refleshCart();
-          } catch (error) {
-              seterrorMessage(error.data.error.message)
-          }
+  const handleAddCart = async (productId, quantity) => {
+    const cart = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  }
 
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart)
+  }
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  }
+  const refleshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+    setCart(newCart);
+  }
+  const handleCapttureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incommingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+      setOrder(incommingOrder);
+      refleshCart();
+    } catch (error) {
+      seterrorMessage(error.data.error.message)
     }
 
-    useEffect(() => {
-      fetchProducts();
-      fetchCart();
-   
-    }, []);
-   
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+
+  }, []);
+
   return (
-    
-    <Router> 
-    <div>
-        <Navbar totalItems ={cart.total_items} ></Navbar>
-       
-      <Routes>
-        <Route path="/" exact element={<Products products={products} onAddToCart = {handleAddCart} />} />
-        <Route path="/Blog" exact element={<Blog />} />
-        <Route path="/cart" exact element={<Cart 
-        cart={cart}
-        handleUpdateCartQty={handleUpdateCartQty }
-        handleRemoveFromCart={handleRemoveFromCart }
-        handleEmptyCart={ handleEmptyCart}
-        />} />
-       <Route path="/checkout" exact element={<Checkout cart={cart}
-       order={order}
-       onCaptureCheckout={handleCapttureCheckout}
-       error={errorMessage}
-        />} />
-       
-        
+
+    <Router>
+      <div>
+        <Navbar totalItems={cart?.total_items} ></Navbar>
+
+        <Routes>
+          <Route path="/" exact element={<Products products={products} onAddToCart={handleAddCart} />} />
+          <Route path="/Blog" exact element={<Blog />} />
+          <Route path="/cart" exact element={<Cart
+            cart={cart}
+            handleUpdateCartQty={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleEmptyCart={handleEmptyCart}
+          />} />
+          <Route path="/checkout" exact element={<Checkout cart={cart}
+            order={order}
+            onCaptureCheckout={handleCapttureCheckout}
+            error={errorMessage}
+          />} />
         </Routes>
 
-     </div>
+      </div>
     </Router>
-  
+
   )
 }
 
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,50 +1,83 @@
-import React from 'react'
-import { Container, Typography, Button, Grid } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-import useStyles from './styles';
-import CartItem from './CartItem/CartItem';
+import useStyles from "./styles";
+import CartItem from "./CartItem/CartItem";
 
-const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart}) => {
-   
-    const classes = useStyles();
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
 
-    const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no ites in your shopping cart. 
-        <Link to="/" className={classes.link}>start adding some!</Link>
+  console.log(cart);
+
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart.
+      <Link to="/" className={classes.link}>
+        start adding some!
+      </Link>
+    </Typography>
+  );
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart?.line_items?.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h6">
+          Subtotal: {cart?.subtotal?.formatted_with_symbol}
         </Typography>
-
-    );
-    const FilledCart =() =>(
-        <>
-        <Grid container spacing={3}>
-            {cart.line_items.map((item) => (
-                <Grid item xs={12} sm={4} key={item.id}>
-                    <CartItem item={item} onUpdateCartQty={handleUpdateCartQty}  onRemoveFromCart = {handleRemoveFromCart}/>
-                </Grid>
-           ) )}
-        </Grid>
-        <div className={classes.cardDetails}>
-        <Typography variant="h6">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
         <div>
-            <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
-            <Button component ={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+            onClick={handleEmptyCart}
+          >
+            Empty Cart
+          </Button>
+          <Button
+            component={Link}
+            to="/checkout"
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
         </div>
-        </div>
-        </>
-        
-    );
-                if(!cart.line_items)
-                return 'Loading...';
+      </div>
+    </>
+  );
 
   return (
-   <Container>
-    <div className={classes.toolbar}/>
-        <div className={classes.toolbar} />
-        <Typography className={classes.title} variant="h3" gutterBottom> Your Shopping cart</Typography>
-        { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
-   </Container>
-  )
-}
+    <Container>
+      <div className={classes.toolbar} />
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        {" "}
+        Your Shopping cart
+      </Typography>
+      {cart?.total_items === 0 || !cart ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
 
-export default Cart
+export default Cart;
